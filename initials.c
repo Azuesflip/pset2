dd8951537d8a41f24aab1309b2859f44e77e2cd1
Bob_Kupfner <azuesflip@gmail.com>
2015-10-16 14:33:38 -0400

    first comit. created the initials program just need to do the other two and document out the file.

---
+++ initials.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <cs50.h>
+#include <ctype.h>
+
+int main(void)
+{
+    printf("Please enter your name: \n");
+    
+    string name = GetString();
+    
+    if(name != NULL)
+    {git remote add origin https://github.com/Azuesflip/pset2.git
+        printf("%c", toupper(name[0]));
+        
+        for( int i = 0; i < strlen(name); i++)
+        {
+            if(name[i] == 32)
+            {
+                printf("%c", toupper(name[i + 1]));
+            }
+        }
+        
+        printf("\n");
+        printf("Thank you there are your initials!\n");
+    }
+}
